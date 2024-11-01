0961a9cba895b71aec16bbed3d6f6b1ad0fd65f6
holysama <97112425+holysama@users.noreply.github.com>
2024-11-01 21:30:27 +0100

    feat: add playRound function
    plays a single round of Rock, Paper and Scissors

--- index.js
+++ index.js
@@ -35,3 +35,28 @@ let humanScore = 0;
 let computerScore = 0;
 
 //Single round of Rock, Paper Scissors logic
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.slice(0, 1).toUpperCase() + humanChoice.slice(1).toLowerCase();
+    computerChoice = computerChoice.slice(0, 1).toUpperCase() + computerChoice.slice(1).toLowerCase();
+
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!");
+    } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+        console.log("User wins! Rock beats Scissors");
+    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+        console.log("User wins! Paper beats Rock");
+    } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+        console.log("User wins! Scissors beats Paper");
+    } else if (computerChoice === "Rock" && humanChoice === "Scissors") {
+        console.log("Computer wins! Rock beats Scissors");
+    } else if (computerChoice === "Paper" && humanChoice === "Rock") {
+        console.log("Computer wins! Paper beats Rock");
+    } else if (computerChoice === "Scissors" && humanChoice === "Paper") {
+        console.log("Computer wins! Scissors beats Paper");
+    }
+}
+
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice(getUserChoice);
+
+playRound(computerSelection, humanSelection);
